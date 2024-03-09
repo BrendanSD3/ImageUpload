7d8088ecc2a3731aab74c0eee7af2155b4ce3b40
Brendan Wall <brendan.wall03@gmail.com>
2024-03-08 16:11:04 -0800

    Darkmode styling

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 // Add some interactivity to the website
-console.log("Hello, world!");
+document.getElementById('btnSwitch').addEventListener('click',()=>{
+    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+        document.documentElement.setAttribute('data-bs-theme','light')
+        document.getElementById('toggle-img').src="images/Moon_fill.svg";
+        document.getElementById('logo').src="images/logo.svg";
+
+    }
+    else {
+        document.documentElement.setAttribute('data-bs-theme','dark');
+        document.getElementById('toggle-img').src="images/Sun_fill.svg";
+        document.getElementById('logo').src="images/logo.svg";
+        document.getElementById('logo').src="images/logo-dark.svg";
+
+
+    }
+})
